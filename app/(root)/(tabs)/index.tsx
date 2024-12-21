c5c56651bceca4ac67a14499f78d481828ce2557
Luong Tu Nhan <tunhan281204@gmail.com>
2024-12-22 01:02:45 +0700

    perf: finish set up project

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Tabs = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text className="font-bold text-4xl text-orange-700">Test</Text>
+      <Text className="font-bold text-4xl text-orange-700 font-rubik">Test</Text>
       <Link href={"/sign-in"}>Sign In</Link>
       <Link href={"/explore"}>Expore</Link>
       <Link href={"/profile"}>Profile</Link>
