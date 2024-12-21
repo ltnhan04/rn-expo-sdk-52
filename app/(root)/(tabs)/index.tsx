9e2c19e90d4634a129261ad97e52708c04c4d72f
Luong Tu Nhan <tunhan281204@gmail.com>
2024-12-22 00:34:26 +0700

    feat: set up native wind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+
+const Tabs = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text className="font-bold text-4xl text-orange-700">Test</Text>
+      <Link href={"/sign-in"}>Sign In</Link>
+      <Link href={"/explore"}>Expore</Link>
+      <Link href={"/profile"}>Profile</Link>
+      <Link href={"/properties/1"}>Property</Link>
+    </View>
+  );
+};
+
+export default Tabs;
