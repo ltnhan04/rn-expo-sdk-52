7f8c764c598179ecf916a46a8a2ed36abece84ac
Luong Tu Nhan <tunhan281204@gmail.com>
2024-12-22 17:39:33 +0700

    feat: set up sdk & auth func

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 
 const SignIn = () => {
+  const handleLogin = async () => {};
   return (
-    <View>
-      <Text>Sign In</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to Real Scout
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}{" "}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Real Scout with Google
+          </Text>
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
