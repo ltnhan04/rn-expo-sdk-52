53b992f11102383e09b466b170a187f110a00f2d
Luong Tu Nhan <tunhan281204@gmail.com>
2024-12-24 01:27:32 +0700

    feat: set up db

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,14 +1,29 @@
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
-import React from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import { Card, FeaturedCard } from "@/components/Cards";
+
+import { useAppwrite } from "@/lib/useAppwrite";
+import { useGlobalContext } from "@/lib/global-provider";
 
 const Home = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text className="font-bold text-4xl text-orange-700 font-rubik">
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <FlatList />
+    </SafeAreaView>
   );
 };
 
